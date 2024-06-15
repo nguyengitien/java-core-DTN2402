fddc770d509cdaede7227d20d44a1749ae601643
nguyengitien <nguyengiactien2001@gmail.com>
2024-06-15 20:07:30 +0700

    add

---
+++ src/main/java/ThrowsDemo.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class ThrowsDemo {
+    public static void main(String[] args) {
+           try{
+
+       }catch ()
+    }
+    public static String readFile() throws IOException{
+        throw new IOException("File không tồn taị");
+    }
+}
---
+++ src/main/java/TryCatchFinallyDemo.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+
+public class TryCatchFinallyDemo {
+    public static void main(String[] args) {
+        try{
+            //ArithmeticException
+            int a=1;
+            int b=0;
+           // System.out.println(a/b);
+
+            //NullPointerException
+            String s=null;
+          //  System.out.println(s.length());
+
+            //
+            int[] array={1,2,3};
+         //   System.out.println(array[100]);
+
+            throw new IOException("lỗi");
+        }catch (ArithmeticException  exception){
+            System.out.println("không thể chia cho 0");
+        }catch (NullPointerException | ArrayIndexOutOfBoundsException exception){
+            System.out.println(exception.getMessage());
+        }catch (Exception exception){
+            System.out.println("đã có lỗi, vui lòng thử lại sau");
+        }finally {
+            System.out.println("khối finally lun được gọi");
+        }
+
+        // try catch
+        //try finally
+        // try catch finally
+    }
+}
