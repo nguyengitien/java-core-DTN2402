6f077592ca743691bc16d764d2aa327b730f0898
nguyengitien <nguyengiactien2001@gmail.com>
2024-06-17 23:41:04 +0700

    add new

--- src/main/java/util/ScannerUtil.java
+++ src/main/java/util/ScannerUtil.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.awt.peer.PanelPeer;
 import java.util.Scanner;
 
 public class ScannerUtil {
@@ -11,8 +12,8 @@ public class ScannerUtil {
                 String input = scanner.nextLine();
                 return Integer.parseInt(input);
             } catch (NumberFormatException exception) {
-                System.out.println("Yêu cầu nhập vào số nguyên.");
-                System.out.println("Nhap lại: ");
+                System.err.println("Yêu cầu nhập vào số nguyên.");
+                System.err.println("Nhập lại:");
             }
         }
     }
@@ -21,15 +22,16 @@ public class ScannerUtil {
         while (true) {
             try {
                 String input = scanner.nextLine();
-                if (Integer.parseInt(input) > 0) {
-                    return Integer.parseInt(input);
+                int n = Integer.parseInt(input);
+                if (n > 0) {
+                    return n;
                 } else {
                     System.err.println("Yêu cầu nhập vào số nguyên dương.");
-                    System.err.println("Nhập lại: ");
+                    System.err.println("Nhập lại:");
                 }
             } catch (NumberFormatException exception) {
-                System.out.println("Yêu cầu nhập vào số nguyên dương.");
-                System.out.println("Nhập lại: ");
+                System.err.println("Yêu cầu nhập vào số nguyên.");
+                System.err.println("Nhập lại:");
             }
         }
     }
@@ -40,20 +42,20 @@ public class ScannerUtil {
                 String input = scanner.nextLine();
                 return Float.parseFloat(input);
             } catch (NumberFormatException exception) {
-                System.out.println("Yêu cầu nhập vào số nguyên.");
-                System.out.println("Nhap lại: ");
+                System.err.println("Yêu cầu nhập vào số thực.");
+                System.err.println("Nhập lại:");
             }
         }
     }
 
-    public static float inputDouble() {
+    public static double inputDouble() {
         while (true) {
             try {
                 String input = scanner.nextLine();
-                return Double.doubleToLongBits(input);
+                return Double.parseDouble(input);
             } catch (NumberFormatException exception) {
-                System.out.println("Yêu cầu nhập vào số nguyên.");
-                System.out.println("Nhap lại: ");
+                System.err.println("Yêu cầu nhập vào số thực.");
+                System.err.println("Nhập lại:");
             }
         }
     }
@@ -63,36 +65,58 @@ public class ScannerUtil {
     }
 
     public static String inputEmail() {
-        // yêu cầu chức @
-        String input = scanner.nextLine();
-        if (input.contains("@")) {
-            return input;
-        } else {
-            System.out.println("Yêu cầu email chứa kí tự @.");
-            System.out.println("Nhập lại: ");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.contains("@")) {
+                return input;
+            } else {
+                System.err.println("Yêu cầu email chứa kí tự @.");
+                System.err.println("Nhập lại:");
+            }
         }
     }
 
     public static String inputPassword() {
-        //yêu cầu 6-12 kí tự, ít nhất 1 kí ự vết hoa
-        String input = scanner.nextLine();
-        int length = input.length();
-        if (length < 6 || length > 12) {
-            System.out.println("Yêu cầu password từ 6 đến 12 kí tu.");
-            System.out.println("Nhập lại");
-        } else {
-            for (int i = 0; i < length; i++) {
-                char c = input.charAt(i);
-                if (Character.isUpperCase(c)) {
-                    return input;
+        while (true) {
+            String input = scanner.nextLine();
+            int length = input.length();
+            if (length < 6 || length > 12) {
+                System.err.println("Yêu cầu password từ 6 đến 12 kí tự.");
+                System.err.println("Nhập lại:");
+            } else {
+                for (int i = 0; i < length; i++) {
+                    char c = input.charAt(i);
+                    if (Character.isUpperCase(c)) {
+                        return input;
+                    }
                 }
+                System.err.println("Yêu cầu password ít nhất 1 kí tự viết hoa.");
+                System.err.println("Nhập lại:");
             }
-            System.out.println("Yêu cầu password ít nhất 1 kí tự viê hoa.");
-            System.out.println("Nhập lại");
         }
     }
 
-    public static String inputFullname(){
-        //yêu cầu chỉ chứa chữ cái (không dặc biet, không số)
+    public static String inputFullName() {
+        while (true) {
+            String input = scanner.nextLine();
+            if (hasAllAlphabetic(input)) {
+                return input;
+            } else {
+                System.err.println("Yêu cầu full name chỉ chứa chữ cái.");
+                System.err.println("Nhập lại:");
+            }
+        }
+    }
+
+    private static boolean hasAllAlphabetic(String s) {
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (Character.isWhitespace(c)) continue;
+            if (!Character.isAlphabetic(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
