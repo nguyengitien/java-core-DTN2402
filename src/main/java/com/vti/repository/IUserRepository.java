b1dfcf627ee1435bae365c137bafc95444410193
nguyengitien <nguyengiactien2001@gmail.com>
2024-07-02 17:57:03 +0700

    add

--- src/main/java/com/vti/repository/IUserRepository.java
+++ src/main/java/com/vti/repository/IUserRepository.java
@@ -9,14 +9,19 @@ import java.util.List;
 public interface IUserRepository {
 
     /**
-     * @pẩm id Định danh user
-    * @return Thông tin user theo id
+    * @return trả về tất cả user trong database
     * @throws SQLException nếu xảy ra lỗi về SQL
      * @throws IOException Nếu xảy ra lỗi cấu hình
     * */
 
     List<User> findAll() throws SQLException, IOException;
 
+    /**
+     * @param id định danh user
+     * @return thông tin user theo id
+     * @throws SQLException nếu xảy ra lỗi về SQL
+     * @throws IOException Nếu xảy ra lỗi cấu hình
+     * */
     User findById(int id) throws SQLException, IOException;
 
     User findByEmailAndPassword(String email, String password) throws SQLException, IOException;
